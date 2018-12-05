c720e9e01ac49368548302c79f697fec6db7a372
ZhijunWei <wzj334965317@outlook.com>
2018-12-04 23:20:51 -0500

    Change openstack-dev to openstack-discuss
    Change-Id: I07e42ffae8cc2d1bbd8f85691ccf5a9373978723

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'openstack-bare-metal'
 maintainer 'openstack-chef'
-maintainer_email 'openstack-dev@lists.openstack.org'
+maintainer_email 'openstack-discuss@lists.openstack.org'
 license 'Apache-2.0'
 description 'Installs/Configures OpenStack Bare Metal service Ironic'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
